2031b5b12db01c5a4602cdd799518f057efd2f43
Jeremiah Pritchard <jeremiah3643@gmail.com>
2018-11-10 19:34:01 -0600

    fixing register

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -13,38 +13,8 @@ export default class Register extends Component {
         values[event.target.id] = event.target.value
         this.setState(values)
     }
-    validate = (name, email, password) => {
-        // we are going to store errors for all fields
-        // in a signle array
-        let errors = [];
-        if (name.length === 0) {
-            errors.push("Name can't be empty");
-        }
-        if (email.length < 5) {
-            errors.push("Email should be at least 5 charcters long");
-        }
-        if (email.split('').filter(x => x === '@').length !== 1) {
-            errors.push("Email should contain a @");
-        }
-        if (email.indexOf('.') === -1) {
-            errors.push("Email should contain at least one dot");
-        }
-        if (password.length < 6) {
-            errors.push("Password should be at least 6 characters long");
-        }
-        this.setState({ errors: errors })
     }
-    handleRegister = () => {
-        let tester1 = this.state.registerEmail
-        let tester2 = this.state.registerPassword
-        let tester3 = this.state.registerUsername
-        this.validate(tester3, tester1, tester2)
-        let errors = this.state.errors
-        debugger
-        if (errors.length) {
-            alert(errors)
-        }
-        else {
+    handleRegister = () => { {
             fetch(`https://serverforum.herokuapp.com/users?email=${this.state.registerEmail}`)
                 .then(r => r.json())
                 .then(user => {
